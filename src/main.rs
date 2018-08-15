a96b09c29b415af45a0258e51a09947808302df6
Russell Blickhan <rwblickhan@gmail.com>
2018-08-15 15:43:27 -0700

    Add sphere with normal vector shading

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,26 @@ extern crate rtracer;
 use rtracer::vec3::Vec3;
 use rtracer::ray::Ray;
 
+fn hit_sphere(center: Vec3, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin() - center;
+    let a = Vec3::dot(r.direction(), r.direction());
+    let b = 2.0 * Vec3::dot(oc, r.direction());
+    let c = Vec3::dot(oc, oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
+}
+
 fn color(r: &Ray) -> Vec3 {
+    let sphere_center = Vec3::new(0.0, 0.0, -1.0);
+    let t = hit_sphere(sphere_center, 0.5, r);
+    if t > 0.0 {
+        let n_hat = Vec3::unit_vector(r.point_at_parameter(t) - sphere_center);
+        return Vec3::new(n_hat.x() + 1.0, n_hat.y() + 1.0, n_hat.z() + 1.0).scale_up(0.5);
+    }
     let unit_direction = Vec3::unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
     Vec3::new(1.0, 1.0, 1.0).scale_up(1.0 - t) + Vec3::new(0.5, 0.7, 1.0).scale_up(t)
